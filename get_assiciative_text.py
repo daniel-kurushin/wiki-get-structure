b6185528ebeb9f3d91dbdd369e676d02637a17db
daniel kurushin <daniel.kurushin@gmail.com>
2019-04-08 11:56:39 +0500

    to ignore

--- get_assiciative_text.py
+++ get_assiciative_text.py
@@ -5,26 +5,28 @@ from get_keywords2 import get_keywords, filter_keywords
 tree = {}
 
 def get_graph(definition = "Дерево", n = 0):
-	global tree
-	if n < 3:
-		definition_text = get_definition(definition)
-		open('/tmp/rez.txt','a').write("=\n%s\n=\n" % definition_text)
+    global tree
+    if n < 3:
+        definition_text = get_definition(definition)
+        open('/tmp/rez.txt','a').write("=\n%s\n=\n" % definition_text)
 
-		keywords = get_keywords(definition_text)
-		keywords = filter_keywords(keywords, set(['гео', 'фам']))
+        keywords = get_keywords(definition_text)
+        
+        print(keywords, definition_text)
 
-		for word in keywords:
-			if word != definition and len(word) > 3:
-				tree.update({(definition, word):1})
-				get_graph(word, n+1)
+        keywords = filter_keywords(keywords, set(['гео', 'фам']))
 
-		#print(tree)
+        for word in keywords:
+            if word != definition and len(word) > 3:
+                tree.update({(definition, word):1})
+                get_graph(word, n+1)
+
 
 if __name__ == '__main__':
-	get_graph("Защита информации", 0)
-	print("digraph g {\n\trankdir=LR;")
-	for definition, word in tree.keys():
-		print("\t\"%s\" -> \"%s\"" % (definition, word))
-	print("}")
+    get_graph("Прикладная лингвистика", 0)
+    print("digraph g {\n\trankdir=LR;")
+    for definition, word in tree.keys():
+        print("\t\"%s\" -> \"%s\"" % (definition, word))
+    print("}")
 	#graph = get_graph(definition = "дерево")
 	# понятие -> понятие2
