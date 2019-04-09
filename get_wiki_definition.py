b95f83811a27592582133645214cf9dce21cf895
daniel kurushin <daniel.kurushin@gmail.com>
2019-04-09 12:49:13 +0500

    rewritten

--- get_wiki_definition.py
+++ get_wiki_definition.py
@@ -1,12 +1,16 @@
-def get_wiki_definition(definition = "Дерево"):
-	
-	from requests import get
-	from bs4 import BeautifulSoup as BS
+from requests import get
+from bs4 import BeautifulSoup as BS
 
-	url = "https://ru.wikipedia.org/wiki/%s" % definition
-	page = get(url).content
-	soup = BS(page, 'html.parser')
-	x = soup.find('div','mw-content-ltr')
-	y = x.find_all('p')
-	myStr = ' '.join([ i.text for i in y ])
-	return myStr
+WIKI_URL = "https://ru.wikipedia.org/wiki/%s"
+
+def get_wiki_definition(word = "Дерево"):
+
+    soup = BS(get(WIKI_URL % word).content, 'html.parser')
+    
+    try:
+        y = soup.find('div','mw-content-ltr').find_all('p')
+        myStr = ' '.join([ i.text for i in y ])
+        return myStr
+    except AttributeError as e:
+        import sys
+        print(word, e, file = sys.stderr)
