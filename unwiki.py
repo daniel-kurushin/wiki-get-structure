1411b64f3338b5a90c5457d78cacacfccc24af3b
Daniel S Kurushin <daniel.kurushin@gmail.com>
2019-04-10 10:08:58 +0500

    regex to remove accents

--- unwiki.py
+++ unwiki.py
@@ -5,10 +5,11 @@ Created on Tue Apr  9 14:05:25 2019
 
 @author: dan
 """
+import re
 
 def unwiki(keywords):
     for word in keywords:
-        yield word.replace(' ́', '').replace(' ','')
+        yield re.sub(r"[^a-z^а-я^-]+", "", word)
         
 if __name__ == '__main__':
     print(list(unwiki(["лингви ́ стика"])))
